a9cf6b3c97f803f0c1070d220edb004435e709ba
Alejandro <hac2803@gmail.com>
2021-05-25 12:23:40 -0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+const user = {
+  firstName: 'Alejandro',
+  lastName: 'Ciravegna',
+  avatar: 'https://watson.mg/wp-content/uploads/2019/12/avatar.png'
+}
+
+function getName(user) {
+  return `${user.firstName} ${user.lastName}` // “template literals” o “template strings”
+}
+
+function App() {
+  // const nombre = 'Alejandro'
+  // const apellido = 'Ciravegna'
+
+  const elemento = (
+    <div>
+      <h1>Hello, {getName(user)}!</h1>  {/* JSX: extensión de JS, no es String ni HTML */}
+      <img src={user.avatar}></img>
+    </div>
+  )
+
+  return (
+    <div className="App">
+      {elemento}
+    </div>);
+}
+
+export default App;
